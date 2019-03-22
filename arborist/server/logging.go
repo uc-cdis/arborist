a552fd88a9e88b90a7e9c6125a2d6108288722b6
Rudyard Richter <rudyardrichter@uchicago.edu>
2019-03-22 14:37:02 -0500

    fix(proxy): fix code for error responses

--- arborist/server/logging.go
+++ arborist/server/logging.go
@@ -20,21 +20,37 @@ func NewLogHandler(out io.Writer, flags int) *LogHandler {
 }
 
 func (handler *LogHandler) Debug(format string, a ...interface{}) {
-	msg := fmt.Sprintf(format, a)
-	handler.logger.Print(fmt.Sprintf("DEBUG: %s", msg))
+	if len(a) > 0 {
+		msg := fmt.Sprintf(format, a)
+		handler.logger.Print(fmt.Sprintf("DEBUG: %s", msg))
+	} else {
+		handler.logger.Print("DEBUG: %s", format)
+	}
 }
 
 func (handler *LogHandler) Info(format string, a ...interface{}) {
-	msg := fmt.Sprintf(format, a)
-	handler.logger.Print(fmt.Sprintf("INFO: %s", msg))
+	if len(a) > 0 {
+		msg := fmt.Sprintf(format, a)
+		handler.logger.Print(fmt.Sprintf("INFO: %s", msg))
+	} else {
+		handler.logger.Print("INFO: %s", format)
+	}
 }
 
 func (handler *LogHandler) Warning(format string, a ...interface{}) {
-	msg := fmt.Sprintf(format, a)
-	handler.logger.Print(fmt.Sprintf("WARNING: %s", msg))
+	if len(a) > 0 {
+		msg := fmt.Sprintf(format, a)
+		handler.logger.Print(fmt.Sprintf("WARNING: %s", msg))
+	} else {
+		handler.logger.Print("WARNING: %s", format)
+	}
 }
 
 func (handler *LogHandler) Error(format string, a ...interface{}) {
-	msg := fmt.Sprintf(format, a)
-	handler.logger.Print(fmt.Sprintf("ERROR: %s", msg))
+	if len(a) > 0 {
+		msg := fmt.Sprintf(format, a)
+		handler.logger.Print(fmt.Sprintf("ERROR: %s", msg))
+	} else {
+		handler.logger.Print("ERROR: %s", format)
+	}
 }
