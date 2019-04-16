0d4f9812d1e86da3483783ee43cd222c389661ee
Rudyard Richter <rudyardrichter@uchicago.edu>
2019-04-16 17:42:37 -0500

    feat(tag): add test

--- arborist/server_test.go
+++ arborist/server_test.go
@@ -404,6 +404,8 @@ func TestServer(t *testing.T) {
 			})
 		})
 
+		var resourceTag string
+
 		t.Run("Create", func(t *testing.T) {
 			w := httptest.NewRecorder()
 			body := []byte(`{"path": "/a"}`)
@@ -428,6 +430,30 @@ func TestServer(t *testing.T) {
 			assert.Equal(t, "a", result.Resource.Name, msg)
 			assert.Equal(t, "/a", result.Resource.Path, msg)
 			assert.NotEqual(t, "", result.Resource.Tag, msg)
+			resourceTag = result.Resource.Tag
+		})
+
+		t.Run("ReadByTag", func(t *testing.T) {
+			w := httptest.NewRecorder()
+			url := fmt.Sprintf("/resource/tag/%s", resourceTag)
+			req := newRequest("GET", url, nil)
+			handler.ServeHTTP(w, req)
+			if w.Code != http.StatusOK {
+				httpError(t, w, "couldn't create resource using tag")
+			}
+			result := struct {
+				Name string `json:"name"`
+				Path string `json:"path"`
+				Tag  string `json:"tag"`
+			}{}
+			err = json.Unmarshal(w.Body.Bytes(), &result)
+			if err != nil {
+				httpError(t, w, "couldn't read response from resource creation")
+			}
+			msg := fmt.Sprintf("got response body: %s", w.Body.String())
+			assert.Equal(t, "a", result.Name, msg)
+			assert.Equal(t, "/a", result.Path, msg)
+			assert.Equal(t, resourceTag, result.Tag, msg)
 		})
 
 		t.Run("CreateSubresource", func(t *testing.T) {
